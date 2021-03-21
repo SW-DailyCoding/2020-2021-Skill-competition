88d60bb2109bd590996abb37e8ea44fdd6d6d18a
jjine <kamisuk@naver.com>
2021-03-22 02:21:01 +0900

    1

---
+++ 2021 지방경기대회/3/htdocs/js/history.js
@@ -0,0 +1,14 @@
+class App {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        
+        localStorage.setItem()
+    }
+}
+
+window.onload = function() {
+    let app = new App();
+}
